671ba1e31b97af2bf76bf545b0391ba70ae41d12
John Browning <johnb@redhat.com>
2021-08-30 16:38:25 -0400

    update clusterenv var

--- image/startnginx.sh
+++ image/startnginx.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 HOSTNAME=`hostname`
+CLUSTERENV=`nslookup $KUBERNETES_SERVICE_HOST | awk -F '=' {'print $2'}`
 echo -e "<footer class='text-center'>\n<p>Served from container: $HOSTNAME running on $CLUSTERENV</p>\n</footer>\n</html>\n" >> index.html
 
 nginx -g "daemon off;"
