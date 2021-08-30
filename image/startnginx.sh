2e7556abe7b0cad67fb723bc6d99b422f7b672b0
John Browning <johnb@redhat.com>
2021-08-30 16:50:50 -0400

    remove clusterenv var

--- image/startnginx.sh
+++ image/startnginx.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 HOSTNAME=`hostname`
-CLUSTERENV=`nslookup $KUBERNETES_SERVICE_HOST | awk -F '=' {'print $2'}`
-echo -e "<footer class='text-center'>\n<p>Served from container: $HOSTNAME running on $CLUSTERENV</p>\n</footer>\n</html>\n" >> index.html
+echo -e "<footer class='text-center'>\n<p>Served from container: $HOSTNAME</p>\n</footer>\n</html>\n" >> index.html
 
 nginx -g "daemon off;"
