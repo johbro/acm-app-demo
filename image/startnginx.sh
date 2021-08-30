aa60ff0ef952bfa1ca7042e088a9cfc2ac4a8005
John Browning <johnb@redhat.com>
2021-08-30 17:08:14 -0400

    updated served-from verbiage

--- image/startnginx.sh
+++ image/startnginx.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 HOSTNAME=`hostname`
-echo -e "<footer class='text-center'>\n<p>Served from container: $HOSTNAME</p>\n</footer>\n</html>\n" >> index.html
+echo -e "<footer class='text-center'>\n<p>Served from pod: $HOSTNAME</p>\n</footer>\n</html>\n" >> index.html
 
 nginx -g "daemon off;"
